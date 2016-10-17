8cec86163b7a88690cf31e6e8008b8932208ee4a
simon <simon.jejcic@gmail.com>
2016-10-17 22:52:29 +0200

    upgrade version

--- upgrades/20161017/__upgrade__.py
+++ upgrades/20161017/__upgrade__.py
@@ -2,7 +2,16 @@
     'version': '20161017',
     'databases': [],  # let's empty to apply to all db
     'description': '',
-    'modules_to_upgrade': [base,],  # to install/update
+    'modules_to_upgrade': [
+	base,
+	louve_custom_pricetag,
+	barcodes_generate,
+ 	coop_shift_state,
+	pos_automatic_cashdrawer,
+	pos_payment_terminal,
+	pos_payment_terminal_return,
+	product_supplierinfo_discount,
+],  # to install/update
     'pre-load': [],  # only .sql
     'post-load': [],  # .sql and .yml
 }
