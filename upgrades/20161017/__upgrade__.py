d974571c0d9acdc1af8880239033c0cd1843a096
simon <simon.jejcic@gmail.com>
2016-10-17 22:39:38 +0200

    update all

--- upgrades/20161017/__upgrade__.py
+++ upgrades/20161017/__upgrade__.py
@@ -2,7 +2,7 @@
     'version': '20161017',
     'databases': [],  # let's empty to apply to all db
     'description': '',
-    'modules_to_upgrade': [],  # to install/update
+    'modules_to_upgrade': [base,],  # to install/update
     'pre-load': [],  # only .sql
     'post-load': [],  # .sql and .yml
 }
