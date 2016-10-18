15b47f4e3c6e6396af02eb8a6f875bf525239ece
jweste <julien.weste@akretion.com.br>
2016-10-18 22:42:16 +0200

    [IMP] New pricetag_vegetable report

--- louve_addons/louve_custom_pricetag/__openerp__.py
+++ louve_addons/louve_custom_pricetag/__openerp__.py
@@ -44,8 +44,13 @@ Copyright, Author and Licence :
         'data/report_paperformat.xml',
         'data/pricetag_model.xml',
         'data/product_category_print.xml',
+        'report/louve_custom_pricetag_report.xml',
         'report/report_pricetag.xml',
+        'report/report_pricetag_vegetables.xml',
         'views/view_product_template.xml',
         'views/view_product_category_print.xml',
+    ],
+    'css': [
+        'static/src/css/pricetag_vegetables.css',
     ],
 }
