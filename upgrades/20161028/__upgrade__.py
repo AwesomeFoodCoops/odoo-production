c1a9663c0d4bff38e92136e957fa35b8dcda5bd8
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-10-28 12:09:09 +0200

    [ADD] new migration script for 28-10-2016

---
+++ upgrades/20161028/__upgrade__.py
@@ -0,0 +1,15 @@
+{
+    'version': '20161028',
+    'databases': [],
+    'description': '',
+    # TODO : Check if it's the good syntax
+    #    'modules_to_uninstall': [
+    #        'louve_custom_pricetag',  # PR #11
+    #    ],
+    'modules_to_upgrade': [
+        'base',                     # Update all
+        'produt_to_scale_bizerba',  # PR #12
+    ],
+    'pre-load': [],
+    'post-load': [],
+}
