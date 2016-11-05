7998f8e24baa25126ed675f896d2575871217dfb
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-11-05 23:43:48 +0100

    [TEST] understanding smile_upgrade

---
+++ upgrades/20161106/__upgrade__.py
@@ -0,0 +1,10 @@
+{
+    'version': '20161106',
+    'databases': [],
+    'description': '',
+    'modules_to_upgrade': [
+        'badge_reader',             # This update introduc a new char field on res.partner
+    ],
+    'pre-load': [],
+    'post-load': [],
+}
