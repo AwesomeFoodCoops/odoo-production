9f2c6a74a2ee9e716af556ec8312988454e387f4
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-11-05 23:22:30 +0100

    [FIX] remove comments because it's a trivial request

--- upgrades/20161104/pre-load.sql
+++ upgrades/20161104/pre-load.sql
@@ -1,5 +1,2 @@
--- Force to recompute cooperative_state field
-ALTER TABLE res_partner DROP column cooperative_state ;
-
--- Update CRON (that is set as noupdate=1)
+ALTER TABLE res_partner DROP column cooperative_state;
 UPDATE ir_cron SET name = 'Partner : Update Working State', function = 'update_working_state' WHERE name = 'Partner : Update Cooperative State';
