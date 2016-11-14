3783d619e12ee1d260c283cba7a4b0ed37ac9fc0
jweste <julien.weste@akretion.com.br>
2016-11-15 00:31:36 +0100

    [IMP]add domain to filter partner_ids who are type A subscriptor for shift.registration, shift_template.registration and shift_template.user_id

--- louve_addons/louve_membership/models/shift_registration.py
+++ louve_addons/louve_membership/models/shift_registration.py
@@ -21,21 +21,11 @@
 #
 ##############################################################################
 
-from openerp import models, api, fields, _
-from openerp.exceptions import ValidationError
+from openerp import models, fields
 
 
-class ShiftTemplateRegistration(models.Model):
-    _inherit = 'shift.template.registration'
+class ShiftRegistration(models.Model):
+    _inherit = 'shift.registration'
 
     partner_id = fields.Many2one(
         domain=[('is_type_A_capital_subscriptor', '=', True)])
-
-    @api.multi
-    @api.constrains('partner_id')
-    def _check_partner_subscription(self):
-        for reg in self:
-            if not reg.partner_id.is_type_A_capital_subscriptor:
-                raise ValidationError(_(
-                    'This partner does not have a type A capital subscription!'
-                ))
