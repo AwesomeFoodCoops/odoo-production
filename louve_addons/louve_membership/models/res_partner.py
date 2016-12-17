ed2a869acaebeebffdb91a3f68c14d943e5f66f4
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-12-17 12:37:59 +0100

    [FIX] flake8 / pylint

--- louve_addons/louve_membership/models/res_partner.py
+++ louve_addons/louve_membership/models/res_partner.py
@@ -3,7 +3,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
+from openerp import models, fields, api
 
 
 EXTRA_COOPERATIVE_STATE_SELECTION = [
