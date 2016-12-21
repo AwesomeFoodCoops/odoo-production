21bbaf2775cc7e8c0e7a6dd0206e3964e0c76010
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-12-21 16:40:42 +0100

    [REF] move coop_shift_state into coop_shift

--- louve_addons/coop_shift/model/__init__.py
+++ louve_addons/coop_shift/model/__init__.py
@@ -32,5 +32,7 @@ from . import shift_type
 from . import shift_ticket
 from . import shift_template_ticket
 from . import shift_mail_registration
+from . import shift_extension
+from . import shift_extension_type
 from . import res_partner
 from . import product
