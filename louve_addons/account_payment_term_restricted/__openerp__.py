3a8c295c2968d15e450a6d4e47a9a496db60b6fb
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-12-22 19:02:52 +0100

    [ADD] new module account_payment_term_restricted

---
+++ louve_addons/account_payment_term_restricted/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': 'Account Payment Terms - Customer / Supplier Restrictions',
+    'version': '9.0.0.0.0',
+    'category': 'Accounting',
+    'author': 'Sylvain LE GAL, La Louve',
+    'website': 'http://www.lalouve.net',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/view_account_payment_term.xml',
+    ],
+}
---
+++ louve_addons/account_payment_term_restricted/models/account_payment_term.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp import models, fields
+
+
+class AccountPaymentTerm(models.Model):
+    _inherit = 'account.payment.term'
+
+    for_customer = fields.Boolean(string='for Customers', default=True)
+
+    for_supplier = fields.Boolean(string='for Suppliers', default=True)
