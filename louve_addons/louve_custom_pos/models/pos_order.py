83068ade7f20afe11852c63151339751e8031e72
yuntux <aurelien@dumaine.me>
2017-04-09 20:25:36 +0200

    Update pos_order.py

--- louve_addons/louve_custom_pos/models/pos_order.py
+++ louve_addons/louve_custom_pos/models/pos_order.py
@@ -9,15 +9,4 @@ from openerp import fields, models
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
-
     amount_total = fields.Float(store=True)
-
-    def _init_amount_total(self, cr, uid, ids=None, context=None):
-        order_ids = self.search(cr, uid, [], context=context)
-        orders = self.browse(cr, uid, order_ids, context=context)
-        for order in orders:
-            currency = order.pricelist_id.currency_id
-            amount_untaxed = currency.round(
-                sum(line.price_subtotal for line in order.lines))
-            order.amount_total = currency.round(
-                order.amount_tax + amount_untaxed)
