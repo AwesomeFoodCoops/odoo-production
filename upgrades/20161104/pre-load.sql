e5045f9f66e74c9b258c26fb430a13039305b76e
yuntux <aurelien@dumaine.me>
2017-06-03 16:48:10 +0200

    remove upgrades folder (link to smile_upgrade)

--- upgrades/20161104/pre-load.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE res_partner DROP column cooperative_state;
-UPDATE ir_cron SET name = 'Partner : Update Working State', function = 'update_working_state' WHERE name = 'Partner : Update Cooperative State';
--- upgrades/20161105/post-load.sql
+++
@@ -1,3 +0,0 @@
-update res_partner set barcode_base = barcode_base_moved0::int where barcode_base_moved0 != '';
-update shift_template set start_datetime = to_timestamp(start_date::text, 'YYYY-MM-DD')  + (start_time - 1) * 3600::text::interval;
-update shift_template set end_datetime = to_timestamp(start_date::text, 'YYYY-MM-DD')  + (end_time - 1) * 3600::text::interval;
