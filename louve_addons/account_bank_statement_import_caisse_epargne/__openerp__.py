1655510f43aa56e8f58c9aefe9a85ce5be98c87a
OpenERP <openerp@lalouve-preprod-host.trobz.com>
2017-08-06 21:54:03 +0000

    New module : account_bank_statement_import_caisse_epargne

---
+++ louve_addons/account_bank_statement_import_caisse_epargne/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Import CSV Bank Statement from Caisse d\'Epargne',
+    'category': 'Banking addons',
+    'version': '9.0.0.0.0',
+    'author': 'Aurélien DUMAINE',
+    'depends': [
+        'account_bank_statement_import',
+    ],
+    'data': [
+        'views/view_account_bank_statement_import.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+}
