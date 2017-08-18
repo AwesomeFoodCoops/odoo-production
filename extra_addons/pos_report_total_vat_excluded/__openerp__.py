3c6a8061710ffeb715eb618922579a68ca34a6ca
Tran Thanh Phuc <phuc@trobz.com>
2017-08-18 14:15:09 +0700

    [IMP] - S#13008 - Move  stored in pos order line to a new module

--- extra_addons/pos_report_total_vat_excluded/__openerp__.py
+++ extra_addons/pos_report_total_vat_excluded/__openerp__.py
@@ -2,25 +2,18 @@
 # Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-
 {
-    'name': 'Louve Custom - Point of Sale Custom views',
+    'name': 'POS Report Total VAT Excluded',
     'version': '9.0.1.0.0',
-    'category': 'Custom',
-    'summary': 'Customize Point of Sale Display',
+    'category': 'Point Of Sale',
+    'summary': 'POS Report Total VAT Excluded',
     'author': 'La Louve',
     'website': 'http://www.lalouve.net',
     'depends': [
-        'point_of_sale',
-        'pos_report_total_vat_excluded'
+        'point_of_sale'
     ],
     'data': [
-        'static/src/xml/templates.xml',
-        'views/view_pos_order.xml',
-    ],
-    'qweb': [
-        'static/src/xml/point_of_sale.xml',
+        'report/report_pos_order_view.xml',
     ],
     'installable': True,
 }
