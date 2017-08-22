7e274a1fba272da1b455bcc1ea2c5d0a99504b6e
Jonathan DHOSY <jonathan.dhosy@smile.fr>
2017-08-22 16:41:19 +0200

    [Imp] Add charges reconciliation

--- louve_addons/bank_auto_reconcille_pos_credit_card_payments/models/account_bank_statement_line.py
+++ louve_addons/bank_auto_reconcille_pos_credit_card_payments/models/account_bank_statement_line.py
@@ -24,22 +24,95 @@ from openerp.exceptions import except_orm
 import re
 from datetime import datetime
 
+remittance_date = False
+remittance_number = False
+
 class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
+
+    def match_line(self, journal, line_name, line_ref, line_note):
+        global remittance_date
+        global remittance_number
+        remittance_date = False
+        remittance_number = False
+        def extract_remittance_date(parser):
+            global remittance_date
+            if remittance_date:
+                return
+            try:
+                res = parser.group("bank_remitance_date")
+                remittance_date = datetime.strptime(res, journal.date_pattern_bank_return).strftime('%Y-%m-%d')
+            except Exception:
+                pass
+
+        def extract_remittance_number(parser):
+            global remittance_number
+            if remittance_number:
+                return
+            try:
+                remittance_number = parser.group("bank_remitance_number")
+            except Exception:
+                pass
+
+        def parse_string(pattern, string):
+            parser_search = re.compile(pattern).search(string)
+
+            if parser_search == None:  # vérification des chaines desquelles ont ne soit rien extraires
+                raise ValueError("The pattern does not match with the string : " + pattern + " => " + string)
+                return False
+
+            extract_remittance_date(parser_search)
+            extract_remittance_number(parser_search)
+
+        parse_string(journal.name_pattern_bank_return, line_name)
+        parse_string(journal.ref_pattern_bank_return, line_ref)
+        if line_note:
+            parse_string(journal.note_pattern_bank_return, line_note)
+
+        return remittance_date, remittance_number
+
+    def match_charges_line(self, journal, line_name, line_ref, line_note):
+        global remittance_number
+        remittance_number = False
+
+        def extract_remittance_number(parser):
+            global remittance_number
+            if remittance_number:
+                return
+            try:
+                remittance_number = parser.group("bank_remitance_number")
+            except Exception:
+                pass
+
+        def parse_string(pattern, string):
+            parser_search = re.compile(pattern).search(string)
+
+            if parser_search == None:  # vérification des chaines desquelles ont ne soit rien extraires
+                raise ValueError("The pattern does not match with the string : " + pattern + " => " + string)
+                return False
+
+            extract_remittance_number(parser_search)
+
+        parse_string(journal.name_charges_pattern_bank_return, line_name)
+        parse_string(journal.ref_charges_pattern_bank_return, line_ref)
+        if line_note:
+            parse_string(journal.note_charges_pattern_bank_return, line_note)
+
+        return remittance_number
 
     @api.multi
     def payment_terminal_bank_reconciliation(self):
         for bank_line in self:
-            name = re.compile(bank_line.journal_id.name_pattern_bank_return).search(bank_line.name or '').string
-            remittance_number = re.compile(bank_line.journal_id.remittance_pattern_bank_return).search(bank_line.remittance_number or '').string
-            ref = re.compile(bank_line.journal_id.ref_pattern_bank_return).search(bank_line.ref or '').string
-            note = re.compile(bank_line.journal_id.note_pattern_bank_return).search(bank_line.note or '').string
-            date = datetime.strptime(bank_line.date, bank_line.journal_id.date_pattern_bank_return).strftime('%Y-%m-%d')
+            if bank_line.amount < 0:
+                continue
+            date, remittance_number = self.match_line(bank_line.journal_id, bank_line.name, bank_line.ref,
+                                                      bank_line.note)
 
-            if remittance_number == '' or date == '':
-                raise except_orm('Warning',_('The current settings do not allow us to deduct the delivery number of this'
-                                        'banking operation. Either this bank account is misconfigured in Odoo,'
-                                        'or it is not a credit card transaction type'))
+            if remittance_number == False or date == False:
+                raise except_orm('Warning',
+                                 _('The current settings do not allow us to deduct the delivery number of this'
+                                   'banking operation. Either this bank account is misconfigured in Odoo,'
+                                   'or it is not a credit card transaction type'))
             journal_id = False
             self._cr.execute("""select sum(amount), journal_id from account_bank_statement_line \
                                 where remittance_number=%s and date=%s and journal_id !=%s group by journal_id \
@@ -48,33 +121,39 @@ class AccountBankStatementLine(models.Model):
             for terminal_line in terminal_line_ids:
                 if terminal_line[0] == bank_line.amount:
                     if journal_id:
-                        raise except_orm('Warning', _('Different journals found with this remittance number and this amount %s' % (str(terminal_line_ids))))
+                        raise except_orm('Warning', _(
+                            'Different journals found with this remittance number and this amount %s' % (
+                            str(terminal_line_ids))))
                     journal_id = terminal_line[1]
             if not journal_id:
-                raise except_orm('Warning', _('Your bank statement indicates an amount of %s %s for the discount number %s.'
-                                             'It is impossible to find a matching discount in our sales history' %
-                                             (bank_line.amount, bank_line.journal_id.currency_id.symbol, remittance_number)))
+                raise except_orm('Warning',
+                                 _('Your bank statement indicates an amount of %s %s for the discount number %s.'
+                                   'It is impossible to find a matching discount in our sales history' %
+                                   (bank_line.amount, bank_line.journal_id.currency_id.symbol, remittance_number)))
 
             terminal_line_ids = self.search([('remittance_number', '=', remittance_number), ('date', '=', date),
                                              ('journal_id', '=', journal_id)])
 
-            reconciled_statement = self.env['account.bank.statement'].search([('all_lines_reconciled', '=', True), ('line_ids', 'in', terminal_line_ids.ids)])
+            reconciled_statement = self.env['account.bank.statement'].search(
+                [('all_lines_reconciled', '=', True), ('line_ids', 'in', terminal_line_ids.ids)])
             if not reconciled_statement:
                 raise except_orm('Warning', _('A statement is not reconciled %s' % (reconciled_statement.ids,)))
 
-            self.reconcil_bank_statement_line(bank_line, remittance_number, terminal_line_ids[0].journal_id.default_credit_account_id.id)
+            self.reconcil_bank_statement_line(bank_line, remittance_number,
+                                              terminal_line_ids[0].journal_id.default_credit_account_id.id)
             self.reconcil_account_move_line(bank_line, terminal_line_ids)
+            self.reconcil_chages_bank_statement_line(bank_line, remittance_number, date)
         return
 
     def reconcil_bank_statement_line(self, bank_line, remittance_number, account_id):
         move_line_data_credit = {
-                'name': _('Remitance BC %s' % (remittance_number,)),
-                'debit': 0.0,
-                'credit': bank_line.amount,
-                'journal_id': bank_line.journal_id.id,
-                'date': bank_line.date,
-                'account_id': account_id,
-                }
+            'name': _('Remitance BC %s' % (remittance_number,)),
+            'debit': 0.0,
+            'credit': bank_line.amount,
+            'journal_id': bank_line.journal_id.id,
+            'date': bank_line.date,
+            'account_id': account_id,
+        }
         bank_line.process_reconciliation([], [], [move_line_data_credit])
         return
 
@@ -82,7 +161,7 @@ class AccountBankStatementLine(models.Model):
         account_id_ref = terminal_line_ids[0].journal_id.default_debit_account_id.id
         line_to_reconcil = []
         for statement_line in terminal_line_ids:
-            for move in statement_line.statement_id.move_line_ids:
+            for move in statement_line.journal_entry_ids.line_ids:
                 if move.account_id.id == account_id_ref and move.id not in line_to_reconcil:
                     line_to_reconcil.append(move.id)
         for move in bank_line.statement_id.move_line_ids:
@@ -90,6 +169,30 @@ class AccountBankStatementLine(models.Model):
                 line_to_reconcil.append(move.id)
         reconcil_obj = self.env['account.move.line.reconcile'].with_context(active_ids=line_to_reconcil)
         reconcil_obj.trans_rec_reconcile_full()
+        return
+
+    def reconcil_chages_bank_statement_line(self, bank_line, remittance_number, date):
+        search_charges = self.search([('amount', '<', 0), ('date', '=', date),
+                                      ('statement_id', '=', bank_line.statement_id.id)])
+        charges_line_id = False
+        for charges_line in search_charges:
+            charges_remittance_number = self.match_charges_line(bank_line.journal_id, charges_line.name,
+                                                                     charges_line.ref, charges_line.note)
+            if charges_remittance_number == remittance_number:
+                if charges_line_id:
+                    raise except_orm('Warning', _(
+                            'Different charges line found with this remittance number %s and this date %s' % (
+                                remittance_number, date)))
+                charges_line_id = charges_line
+        move_line_data_credit = {
+            'name': _('Remitance BC %s' % (remittance_number,)),
+            'debit': charges_line_id.amount * -1,
+            'credit': 0.0,
+            'journal_id': bank_line.journal_id.id,
+            'date': bank_line.date,
+            'account_id': bank_line.journal_id.bank_charge_account_id.id,
+        }
+        charges_line_id.process_reconciliation([], [], [move_line_data_credit])
         return
 
 
--- louve_addons/bank_auto_reconcille_pos_credit_card_payments/models/account_journal.py
+++ louve_addons/bank_auto_reconcille_pos_credit_card_payments/models/account_journal.py
@@ -26,8 +26,10 @@ class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
     name_pattern_bank_return = fields.Char('Name pattern', default='.*')
-    remittance_pattern_bank_return = fields.Char('Remittance pattern', default='.*')
     ref_pattern_bank_return = fields.Char('Ref pattern', default='.*')
     note_pattern_bank_return = fields.Char('Note pattern', default='.*')
     date_pattern_bank_return = fields.Char('Date pattern', default='%Y-%m-%d')
+    name_charges_pattern_bank_return = fields.Char('Name charges pattern', default='.*')
+    ref_charges_pattern_bank_return = fields.Char('Ref charges pattern', default='.*')
+    note_charges_pattern_bank_return = fields.Char('Note charges pattern', default='.*')
     bank_charge_account_id = fields.Many2one('account.account', 'Bank charge account')
