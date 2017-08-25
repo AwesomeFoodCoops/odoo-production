aa5e689d6666df57b851722469e022b59694d212
Tran Thanh Phuc <phuc@trobz.com>
2017-08-25 15:48:12 +0700

    [ADD] - S#13907 - Improvements for automatic unsubscription

---
+++ louve_addons/louve_custom_email/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: La Louve
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+
+{
+    'name': 'Louve Custom - Email',
+    'version': '9.0.1.0.2',
+    'category': 'Custom',
+    'author': 'La Louve',
+    'website': 'http://www.lalouve.net',
+    'license': 'AGPL-3',
+    'depends': [
+        'coop_membership',
+    ],
+    'data': [
+        'data/email_template_data.xml',
+    ],
+}
