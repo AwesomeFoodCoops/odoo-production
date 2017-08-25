0325aea5c2ed568861aef39497bc0e390fdd3802
Tran Thanh Phuc <phuc@trobz.com>
2017-08-25 15:48:12 +0700

    [ADD] - S#13907 - Improvements for automatic unsubscription

--- intercoop_addons/coop_membership/models/shift_registration.py
+++ intercoop_addons/coop_membership/models/shift_registration.py
@@ -82,3 +82,52 @@ class ShiftRegistration(models.Model):
             }
             res_extension = shift_extension_env.create(ext_vals)
             registration.related_extension_id = res_extension.id
+
+    @api.multi
+    def button_reg_absent(self):
+        res = super(ShiftRegistration, self).button_reg_absent()
+        for reg in self:
+            # Terminate the shift member
+            if reg.template_created and reg.shift_type == 'standard':
+                # Check for his next last shift registration
+                last_shift_reg = self.search(
+                    [('partner_id', '=', reg.partner_id.id),
+                     ('template_created', '=', True),
+                     ('date_begin', '<', reg.date_begin),
+                     ('shift_type', '=', 'standard')],
+                    order='date_begin desc', limit=1)
+                if last_shift_reg and last_shift_reg.state == 'absent':
+                    # Check for any standard shift within
+                    markup_shift_reg_count = self.search_count(
+                        [('partner_id', '=', reg.partner_id.id),
+                         ('date_begin', '<', last_shift_reg.date_begin),
+                         ('date_begin', '>', reg.date_begin),
+                         ('shift_type', '=', 'standard'),
+                         ('state', 'in', ('done', 'replaced'))])
+                    # If no markup reg found, set date end for the reg shift
+                    if not markup_shift_reg_count:
+                        tz_name = self._context.get('tz') or self.env.user.tz
+                        date_end_obj = datetime.strptime(
+                            reg.date_end, '%Y-%m-%d %H:%M:%S')
+                        utc_timestamp = pytz.utc.localize(
+                            date_end_obj, is_dst=False)
+                        context_tz = pytz.timezone(tz_name)
+                        reg_date_end_tz = utc_timestamp.astimezone(context_tz)
+
+                        final_date_end = reg_date_end_tz.strftime('%Y-%m-%d')
+                        reg.tmpl_reg_line_id.date_end = final_date_end
+
+                        # Sending the email to notify the member
+                        partner = reg.partner_id
+
+                        # Get the template xml from context
+                        template_name = self.env.context.get(
+                            "unsubscribe_email_template", False)
+                        if not template_name:
+                            template_name = "coop_membership.unsubscribe_email"
+                        mail_template = \
+                            self.env.ref(template_name)
+                        if not mail_template:
+                            continue
+                        mail_template.send_mail(partner.id)
+        return res
