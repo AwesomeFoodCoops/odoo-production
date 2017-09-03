08b58c2b3a749a36c1339a99dd402ba09475efdc
yuntux <yuntux@gmail.com>
2017-09-03 23:04:24 +0200

    Update account_bank_statement_import.py

--- louve_addons/account_bank_statement_import_caisse_epargne/models/account_bank_statement_import.py
+++ louve_addons/account_bank_statement_import_caisse_epargne/models/account_bank_statement_import.py
@@ -20,13 +20,11 @@ class AccountBankStatementImport(models.TransientModel):
         try:
             parse_line_1 = re.compile(u"^Code de la banque : (?P<bank_group_code>\d{5});Code de l'agence : (?P<bank_local_code>\d{5});Date de début de téléchargement : (?P<opening_date>\d{2}/\d{2}/\d{4});Date de fin de téléchargement : (?P<closing_date>\d{2}/\d{2}/\d{4});;$").search(data_file[0])
             bank_group_code = parse_line_1.group('bank_group_code')
-            bank_local_code = parse_line_1.group('bank_local_code')
             openning_date = parse_line_1.group('opening_date')
             closing_date = parse_line_1.group('closing_date')
 
             parse_line_2 = re.compile(u"^Numéro de compte : (?P<bank_account_number>\d{11});Nom du compte : (?P<bank_account_name>.*);Devise : (?P<currency>.{3});;;$").search(data_file[1])
             bank_account_number = parse_line_2.group('bank_account_number')
-            bank_account_name = parse_line_2.group('bank_account_name')
             currency = parse_line_2.group('currency')
 
             closing_balance = float(re.compile(u"^Solde en fin de période;;;;(?P<balance>\d+(,\d{1,2})?);$").search(data_file[3]).group('balance').replace(',','.'))
@@ -35,7 +33,7 @@ class AccountBankStatementImport(models.TransientModel):
         except Exception as e:
             _logger.debug(e)
             return False
-        return (bank_group_code,bank_local_code,openning_date,closing_date,bank_account_number,bank_account_name,opening_balance,closing_balance,currency)
+        return (bank_group_code,openning_date,closing_date,bank_account_number,opening_balance,closing_balance,currency)
 
     @api.model
     def _parse_file(self, data_file):
@@ -45,7 +43,7 @@ class AccountBankStatementImport(models.TransientModel):
             return super(AccountBankStatementImport, self)._parse_file(
                 data_file)
 
-        bank_group_code,bank_local_code,openning_date,closing_date,bank_account_number,bank_account_name,opening_balance,closing_balance,currency = result
+        bank_group_code,openning_date,closing_date,bank_account_number,opening_balance,closing_balance,currency = result
         transactions = []
         total_amt = 0.00
         try:
