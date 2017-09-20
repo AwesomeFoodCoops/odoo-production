c2079dec8e13b341e05803331f2e54ec41ba542d
Nguyen Van Thong <thongnv@trobz.com>
2017-09-20 18:12:14 +0700

    [ADD] - S#14053 - POS: move custom qweb views from louve_custom_pos to new intercoop module

--- intercoop_addons/coop_point_of_sale/__openerp__.py
+++ intercoop_addons/coop_point_of_sale/__openerp__.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
-# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author: La Louve
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
 {
-    'name': 'Louve Custom - Point of Sale Custom views',
-    'version': '9.0.1.0.0',
+    'name': 'Coop - Point of Sale Custom views',
+    'version': '9.0.2.0.0',
     'category': 'Custom',
     'summary': 'Customize Point of Sale Display',
     'author': 'La Louve',
     'website': 'http://www.lalouve.net',
     'depends': [
         'point_of_sale',
-        'pos_report_total_vat_excluded',
-        'coop_point_of_sale',
+        'coop_membership',
+    ],
+    'qweb': [
+        'static/src/xml/point_of_sale.xml',
     ],
     'data': [
         'static/src/xml/templates.xml',
+        'views/view_pos_order.xml',
     ],
     'installable': True,
 }
