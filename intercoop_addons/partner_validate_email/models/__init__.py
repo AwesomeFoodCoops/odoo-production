bcc416371acc7c601393929cd5aaf835aa4be487
Tran Thanh Phuc <phuc@trobz.com>
2017-09-27 17:13:29 +0700

    [ADD] - S#13474 - Add new modules for validating email.

---
+++ intercoop_addons/partner_validate_email/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: La Louve (<http://www.lalouve.net/>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import res_config
+from . import res_partner
