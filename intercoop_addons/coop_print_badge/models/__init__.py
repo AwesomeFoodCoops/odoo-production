c51932a653440a935443ea7c25c00c0e45eb9652
Tran Thanh Phuc <phuc@trobz.com>
2017-10-19 18:46:21 +0700

    [IMP] - S#14377 - Badge to print again : wrong criterions

--- intercoop_addons/coop_print_badge/models/__init__.py
+++ intercoop_addons/coop_print_badge/models/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from . import res_partner
+from . import members_config_settings
---
+++ intercoop_addons/coop_print_badge/models/members_config_settings.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: La Louve (<http://www.lalouve.net/>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp import models, fields, api
+from openerp.tools.safe_eval import safe_eval
+
+
+class MemberConfigSettings(models.TransientModel):
+    _inherit = 'members.config.settings'
+
+    reprint_change_field_ids = fields.Many2many(
+        'ir.model.fields', string="Fields trigger badge reprinting",
+        domain=[('model_id.model', '=', 'res.partner')])
+
+    @api.multi
+    def set_params(self):
+        self.ensure_one()
+        value = self.reprint_change_field_ids.ids
+        self.env['ir.config_parameter'].set_param(
+            'reprint_change_field_ids', value)
+
+    @api.multi
+    def get_default_params(self):
+        field_str = self.env['ir.config_parameter'].get_param(
+            'reprint_change_field_ids', '[]')
+        field_ids = safe_eval(field_str)
+        return {
+            'reprint_change_field_ids': field_ids
+        }
