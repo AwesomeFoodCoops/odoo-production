a9057a2f2bdfaea5ae0efd1934f8244c7c6659e3
Tran Thanh Phuc <phuc@trobz.com>
2017-10-20 15:23:32 +0700

    [ADD] - S#13994 - On invoice line, get package qty and discount of product, calculate total amount

---
+++ intercoop_addons/coop_purchase/models/account_invoice_line.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, api, fields
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    base_price = fields.Monetary("Base Price")
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        ret = super(AccountInvoiceLine, self)._onchange_product_id()
+        if self.invoice_id.type in ('in_invoice', 'in_refund') and \
+                self.product_id:
+            suppliers = self.product_id.seller_ids.filtered(
+                lambda x: x.name == self.partner_id)
+            if suppliers:
+                self.package_qty = suppliers[0].package_qty
+                self.discount = suppliers[0].discount
+                self.base_price = suppliers[0].base_price
+        return ret
