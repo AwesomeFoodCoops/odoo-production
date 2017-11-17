f78dcd54b7a634db31961e0233592efa2e92a618
jssuzanne <jssuzanne@anybox.fr>
2017-11-17 15:44:04 +0100

    #9695: Update update_attachment

--- foodcoops_upgrade.py
+++ foodcoops_upgrade.py
@@ -2,10 +2,12 @@
 
 
 def update_attachment(session, logger, ins_modules, up_modules):
-    if session.db_version < (1, 1, 0):
+    """Install large object in the db"""
+    if session.db_version < '1.1' or not session.db_version:
         ins_modules.add('attachment_large_object')
         parameter = session.registry('ir.config_parameter')
         logger.info('Setup paramter to use advanced attachment')
         parameter.set_param(
             session.cr, session.uid, 'ir_attachment.location',
             'postgresql:lobject')
+        session.cr.commit()
