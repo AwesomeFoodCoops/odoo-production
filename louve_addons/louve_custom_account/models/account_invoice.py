ed081489bf39416727eb3d41c85361f9cec44a27
tinvan94 <tinvan111294@gmail.com>
2018-01-19 15:20:18 +0700

    [ADD][F#25851][S#15491]Group by article in invoice line

---
+++ louve_addons/louve_custom_account/models/account_invoice.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def merge_lines(self):
+        for invoice in self:
+            to_delete_ids = []
+            itered_ids = []
+            for line in invoice.invoice_line_ids:
+                itered_ids.append(line.id)
+                line_to_merge = invoice.invoice_line_ids.search([
+                    ('invoice_id', '=', invoice.id),
+                    ('discount', '=', line.discount),
+                    ('price_unit', '=', line.price_unit),
+                    ('product_id', '=', line.product_id.id),
+                    ('account_id', '=', line.account_id.id),
+                    ('account_analytic_id', '=', line.account_analytic_id.id),
+                    ('id', 'not in', itered_ids)], limit=1)
+                if line_to_merge:
+                    to_delete_ids.append(line.id)
+                    line_to_merge.write({
+                        'quantity': line_to_merge.quantity + line.quantity,
+                        'origin': (line_to_merge.origin or
+                        '') + (line.origin or '')})
+            invoice.write({'invoice_line_ids':
+                           [(2, id_delete) for id_delete in to_delete_ids]})
