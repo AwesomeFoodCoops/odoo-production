527e7e12722278e1490022810b6d9d2d3fae5d1f
tinvan94 <tinvan111294@gmail.com>
2018-02-05 11:36:22 +0700

    [RFF][F#25676][S#15489][Check deposit] Member not cheque holder

--- extra_addons/account_check_deposit/models/account_deposit.py
+++ extra_addons/account_check_deposit/models/account_deposit.py
@@ -272,7 +272,7 @@ class AccountMoveLine(models.Model):
         compute='compute_state_deposit',
         help="Technical fields use invisible button 'DELETE'"
         "if state is draft button visible else button invisible")
-    check_holder_name = fields.Char(string="Cheque Holder")        
+    check_holder_name = fields.Char(string="Cheque Holder")
 
     @api.multi
     def compute_state_deposit(self):
@@ -290,28 +290,33 @@ class AccountMoveLine(models.Model):
 
     @api.onchange('partner_id')
     def onchange_partner_id(self):
-        if self.partner_id:
-            self.check_holder_name = self.partner_id.name_get()[0][1] or ''
+        change_check_holder_name = self._context.get(
+            'change_check_holder_name', False)
+        if self.partner_id and not self.check_holder_name:
+            if change_check_holder_name or self.check_deposit_id:
+                self.check_holder_name = self.partner_id.name_get()[0][1] or ''
 
     @api.model
     def create(self, vals):
-        partner_id = vals.get('partner_id', False)
-        check_holder_name = vals.get('check_holder_name', False)
-        partner = self.env['res.partner'].browse(partner_id)
-        if partner and not check_holder_name:
-            vals.update({
-                'check_holder_name': partner.name_get()[0][1] or ''
-            })
-        return super(AccountMoveLine, self).create(vals)
+        res = super(AccountMoveLine, self).create(vals)
+        res.update_check_holder_name()
+        return res
 
     @api.multi
     def write(self, vals):
-        if 'partner_id' in vals and 'check_holder_name' not in vals:
-            partner_id = vals.get('partner_id', False)
-            for record in self:
-                partner = self.env['res.partner'].browse(partner_id)
-                if partner:
-                    vals.update({
-                        'check_holder_name': partner.name_get()[0][1] or ''
-                    })
-        return super(AccountMoveLine, self).write(vals)
+        res = super(AccountMoveLine, self).write(vals)
+        self.update_check_holder_name()
+        return res
+
+    @api.multi
+    def update_check_holder_name(self):
+        '''
+            Update check holder name for item was generate from deposit
+        '''
+        for record in self:
+            if record.check_deposit_id and record.partner_id and not\
+                    record.check_holder_name:
+                record.check_holder_name = record.partner_id.name_get()[
+                    0][1] or ''
+            elif not record.check_deposit_id and record.check_holder_name:
+                record.check_holder_name = ""
