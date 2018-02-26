1f838291ad194181979b29f786732e846b89d11d
tinvan94 <tinvan111294@gmail.com>
2018-02-26 17:05:05 +0700

    [ADD][F#26676][S#16094]ASSET start date [P1]

--- intercoop_addons/coop_account/models/__init__.py
+++ intercoop_addons/coop_account/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import account
 from . import account_move_line
+from . import account_invoice_line
---
+++ intercoop_addons/coop_account/models/account_invoice_line.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api, models
+from datetime import datetime
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as DF
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    @api.one
+    @api.depends('asset_category_id', 'invoice_id.date_invoice')
+    def _get_asset_date(self):
+        super(AccountInvoiceLine, self)._get_asset_date()
+        cat = self.asset_category_id
+        if cat:
+            if self.invoice_id.date_invoice:
+                start_date = datetime.strptime(
+                    self.invoice_id.date_invoice, DF)
+                self.asset_start_date = start_date.strftime(DF)
