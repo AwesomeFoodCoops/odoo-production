f41f01e63d9a1baf1c05f2216acac99eeb0cb864
tinvan94 <tinvan111294@gmail.com>
2018-03-19 18:23:22 +0700

    [ADD][F#27365][S#16807]Unreconcile account.moves

--- intercoop_addons/coop_account/models/__init__.py
+++ intercoop_addons/coop_account/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import account
+from . import account_move
 from . import account_move_line
 from . import account_invoice_line
 from . import account_bank_statement_line
