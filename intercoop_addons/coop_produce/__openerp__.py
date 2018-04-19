ec2c5ad9c7e00a30824b5df1f343c4f950acd520
root <rihene.abdelwahed@see-tek.com>
2018-04-19 20:37:28 +0100

    Correctif Bugs + Traduction + Ajout Fonctionalités Historique produit et lignes pdts planification

---
+++ intercoop_addons/coop_produce/__openerp__.py
@@ -0,0 +1,29 @@
+#-*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Coop Produce',
+    'version': '9.0.1',
+    'category': 'Warehouse',
+    'sequence': 38,
+    'description': """
+
+Le module permet d’effectuer des inventaires des fruits & légumes dans le colisage de référence des produits.  
+Il offre à cet effet les fonctionnalités suivantes :
+
+	* Initialiser un inventaire rapidement des fruits&légumes depuis des filtres prédéfinies sur l’écran d’inventaire
+	* De générer une planification hebdomadaire depuis la liste de produits inventoriés
+    * De planifier les quantités à commande par jour de la semaine, par produit et par fournisseur
+    * De générer les commandes par fournissseur et par jour.
+
+    """,
+    'depends': [
+        'base',
+        'stock',
+        'purchase',
+    ],
+    'data': [
+        'views/product_views.xml',
+        'views/stock_views.xml',
+    ],
+}
