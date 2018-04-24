85db52470aac6a8abdd3b1c6bc582faa46342cb1
tran quoc thai <thaitq@trobz.com>
2018-04-24 11:56:14 +0700

    [FIX] - S#15535 - F#25720 - louve_date_search_extended

---
+++ intercoop_addons/date_search_extended/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import account_move
+from . import account_move_line
+from . import pos_order
+from . import pos_session
---
+++ intercoop_addons/date_search_extended/models/account_move.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from datetime import datetime
+from openerp import models, fields, api
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    search_year = fields.Char(
+        string='Year (Search)', compute='_compute_date_search',
+        multi='_date_search', store=True, index=True)
+
+    search_month = fields.Char(
+        string='Month (Search)', compute='_compute_date_search',
+        multi='_date_search', store=True, index=True)
+
+    search_day = fields.Char(
+        string='Day (Search)', compute='_compute_date_search',
+        multi='_date_search', store=True, index=True)
+
+    @api.multi
+    @api.depends('date')
+    def _compute_date_search(self):
+        date_scheme = '%Y-%m-%d'
+        for item in self:
+            if item.date:
+                my_date = datetime.strptime(item.date, date_scheme)
+                item.search_year = my_date.strftime('%Y')
+                item.search_month = my_date.strftime('%Y-%m')
+                item.search_day = my_date.strftime('%Y-%m-%d')
+            else:
+                item.search_year = False
+                item.search_month = False
+                item.search_day = False
