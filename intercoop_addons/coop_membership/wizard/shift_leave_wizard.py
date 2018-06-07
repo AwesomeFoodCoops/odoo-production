95cc3701c13b862fb1d720ec837ed55401782880
Nguyen Van Tin <tinnv@trobz.com>
2018-06-07 16:51:28 +0700

    [ADD][F#28675][S#17773]ADD ANOTHER AUTOMATIC MESSAGE TO NON-DEFINED LEAVES

--- intercoop_addons/coop_membership/wizard/shift_leave_wizard.py
+++ intercoop_addons/coop_membership/wizard/shift_leave_wizard.py
@@ -13,8 +13,17 @@ class ShiftLeaveWizard(models.TransientModel):
 
         for wiz in self:
             leave = wiz.leave_id
-            if not (leave.type_id and leave.type_id.is_temp_leave):
+
+            if leave.non_defined_type and leave.non_defined_leave:
+                tmpl_name = 'coop_membership.confirm_leave_non_define_email'
+                mail_tmpl = self.env.ref(tmpl_name)
+                if mail_tmpl:
+                    mail_tmpl.send_mail(leave.id)
+                return res
+
+            elif not (leave.type_id and leave.type_id.is_temp_leave):
                 continue
+
             if not tmpl_name:
                 tmpl_name = leave.partner_id.in_ftop_team and \
                     'coop_membership.coop_ftop_leave_email' or \
