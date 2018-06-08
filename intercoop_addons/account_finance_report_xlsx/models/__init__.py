d8f9e4bcc8b95083ef21e6ac36cdfcf762d71048
Nguyen Van Tin <tinnv@trobz.com>
2018-06-08 19:00:26 +0700

    [ADD][F#28388][S#17488]Excel format for financial reports

---
+++ intercoop_addons/account_finance_report_xlsx/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.fr/>)
+# @author: La Louve
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+
+from . import report_finance_format
+from . import res_company
