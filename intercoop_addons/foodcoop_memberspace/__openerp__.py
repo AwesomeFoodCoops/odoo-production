667db933a1de2c731de5ad7f092e0f6063d56681
Nguyen Anh Tuan <tuanna@trobz.com>
2018-07-27 10:00:33 +0700

    [ADD] Init branch member-space

---
+++ intercoop_addons/foodcoop_memberspace/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017-Today: La Louve (<http://www.lalouve.net/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': 'FoodCoop Memberspace',
+    'version': '0.0.0.1',
+    'category': 'Custom',
+    'author': 'La Louve',
+    'website': 'http://www.lalouve.net',
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'data/website_menu.xml',
+        'views/templates.xml',
+        'views/website/my_work.xml',
+        'views/website/my_team.xml',
+        'views/website/website_homepage.xml',
+        'views/website/website_template.xml',
+    ],
+    'installable': True,
+}
---
+++ intercoop_addons/foodcoop_memberspace/controllers/main.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+import openerp
+from openerp.addons.web import http
+from openerp.http import request
+from openerp import tools
+from datetime import datetime
+import pytz
+import locale
+
+
+class Website(openerp.addons.website.controllers.main.Website):
+
+    @http.route('/', type='http', auth="public", website=True)
+    def index(self, **kw):
+        user = request.env.user
+        shift_registration_env = request.env['shift.registration']
+        shift = shift_registration_env.sudo().search(
+            [('partner_id', '=', user.partner_id.id),
+             ('state', '!=', 'cancel'),
+             ('date_begin', '>=', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))],
+            order="date_begin", limit=1
+        )
+        lang = user.lang and (str(user.lang) + '.utf8') or 'fr_FR.utf8'
+        locale.setlocale(locale.LC_TIME, lang)
+        user_tz = user.tz or 'Europe/Paris'
+        local = pytz.timezone(user_tz)
+        date_begin = shift and datetime.strftime(pytz.utc.localize(
+            datetime.strptime(
+            shift.date_begin, "%Y-%m-%d %H:%M:%S")).astimezone(
+            local),"%A, %B %d %Hh%M") or False
+
+        local_dt = local.localize(datetime.strptime(
+            '2018-06-29 18:17:00', '%Y-%m-%d %H:%M:%S'))
+        start_local_native_dt = datetime.strptime(
+            local_dt.strftime('%Y-%m-%d 00:00:00'), '%Y-%m-%d %H:%M:%S')
+        start_local_tz_dt = local.localize(start_local_native_dt)
+        start_utc_dt = start_local_tz_dt.astimezone(pytz.utc)
+
+        turnover_the_day = request.env['pos.order'].sudo().search(
+            [('state', '=', 'paid'),
+             ('date_order', '>=', start_utc_dt.strftime('%Y-%m-%d %H:%M:%S')),
+             ('date_order', '<=', datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))]
+        )
+        turnover_the_day = sum(item.amount_total for item in turnover_the_day)
+        print '============= turnover_the_day', turnover_the_day
+        values = {
+            'date_begin': date_begin and date_begin.title() or False,
+            'shift': shift,
+            'turnover_the_day': turnover_the_day
+        }
+        return http.request.render('foodcoop_memberspace.homepage', values)
+
+    @http.route(website=True, auth="public")
+    def web_login(self, redirect=None, *args, **kw):
+        r = super(Website, self).web_login(redirect=redirect, *args, **kw)
+        if not redirect and request.params['login_success']:
+            redirect = '/'
+            return http.redirect_with_hash(redirect)
+        return r
+
+    @http.route('/mywork', type='http', auth='user', website=True)
+    def page_mywork(self, **kwargs):
+        user = request.env.user
+        # check standard member or ftop member
+        return request.render(
+            'foodcoop_memberspace.mywork',
+            {
+                'is_standard_member': False,
+                'is_ftop_member': True
+            }
+        )
+
+    @http.route('/standard/counter_classic', type='http', auth='user', website=True)
+    def page_counter_classic(self, **kwargs):
+        user = request.env.user
+        # check standard member or ftop member
+        return request.render(
+            'foodcoop_memberspace.counter',
+            {
+                'is_standard_member': True,
+                'classic': True
+            }
+        )
+
+    @http.route('/standard/counter_extra', type='http', auth='user', website=True)
+    def page_counter_extra(self, **kwargs):
+        user = request.env.user
+        # check standard member or ftop member
+        return request.render(
+            'foodcoop_memberspace.counter',
+            {
+                'is_standard_member': True,
+                'extra': True
+            }
+        )
+
+    @http.route('/standard/programmer_un_extra', type='http', auth='user', website=True)
+    def page_programmer_un_extra(self, **kwargs):
+        user = request.env.user
+        # check standard member or ftop member
+        return request.render(
+            'foodcoop_memberspace.counter',
+            {
+                'is_standard_member': True,
+                'programmer_un_extra': True
+            }
+        )
+
+    @http.route('/standard/echange_de_services', type='http', auth='user', website=True)
+    def page_echange_de_services(self, **kwargs):
+        user = request.env.user
+        # check standard member or ftop member
+        return request.render(
+            'foodcoop_memberspace.counter',
+            {
+                'is_standard_member': True,
+                'echange_de_services': True
+            }
+        )
+
+    @http.route('/ftop/my_counter', type='http', auth='user', website=True)
+    def page_ftop_my_counter(self, **kwargs):
+        user = request.env.user
+        # check standard member or ftop member
+        return request.render(
+            'foodcoop_memberspace.ftop_my_counter',
+            {
+                'is_ftop_member': True,
+            }
+        )
+
+    @http.route('/myteam', type='http', auth='user', website=True)
+    def page_myteam(self, **kwargs):
+        user = request.env.user
+        # check standard member or ftop member
+        return request.render(
+            'foodcoop_memberspace.myteam',
+            {}
+        )
