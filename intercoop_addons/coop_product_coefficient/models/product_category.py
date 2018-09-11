66285c2c1ce5d4942098e0f0e84deb2761a2bdeb
Nguyen Van Tin <tinnv@trobz.com>
2018-09-11 15:07:31 +0700

    [ADD][F#29821][S#18533]Alphabetize product categories

---
+++ intercoop_addons/coop_product_coefficient/models/product_category.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.fr/>)
+# @author: La Louve
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+
+from openerp import api, fields, models
+
+
+class ProductCategory(models.Model):
+    _inherit = "product.category"
+    _order = "name"
