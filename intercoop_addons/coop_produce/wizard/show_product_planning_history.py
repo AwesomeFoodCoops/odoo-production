67e1bc8629a4ed6e691cacbabba4de3c7f6d720c
sarac2014 <samir.rachedi@gmail.com>
2018-09-16 20:24:04 +0200

    [IMP] Set new wizard to view product history

--- intercoop_addons/coop_produce/wizard/show_product_planning_history.py
+++ intercoop_addons/coop_produce/wizard/show_product_planning_history.py
@@ -32,13 +32,25 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-class PlanificationHistoriqueProduit(models.TransientModel):
+class PlanificationProductHistory(models.TransientModel):
     _name = "planification.product.history"
     _description = "Product history planning"
 
     product_id = fields.Many2one('product.product', 'Product', required=True)
-    default_packaging = fields.Float('Default packaging', digits_compute=dp.get_precision('Product Unit of Measure'))
-    line_ids = fields.Many2many('order.week.planning.line')
+    default_packaging = fields.Float('Default packaging', related='product_id.default_packaging')
+    line_ids = fields.Many2many('order.week.planning.line', string="History")
+
+    @api.model
+    def default_get(self, fields):
+        context = self._context or {}
+        res = super(PlanificationProductHistory, self).default_get(fields)
+        line_ids = self.env.context.get('line_ids',[])
+        product_id = self.env.context.get('product_id',False)
+        res.update({
+                'product_id':product_id,
+                'line_ids':[(6,0,line_ids)]
+        })
+        return res
 
     @api.multi
     def init_display_from_date(self,date):
