875e0d2bd157b66d2f82747d3bff286f180705f7
Nguyen Van Tin <tinnv@trobz.com>
2018-09-26 13:21:43 +0700

    [ADD][F#28089][S#17379]Search Limit Days

--- intercoop_addons/coop_account/models/account_journal.py
+++ intercoop_addons/coop_account/models/account_journal.py
@@ -7,5 +7,8 @@ class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
     search_limit_days = fields.Integer(
-        string='Search Limit Days', default=0
+        string='Search Limit Days',
+        help="Set here the number of days before and after the bank " +
+        "transaction on which Journal Items can be proposed for reconciliation",
+        default=0
     )
