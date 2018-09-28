7fc966c5cef6f757b5e447eaf53ec408cc9e16a2
Diep Huu Hoang <hoang@trobz.com>
2018-09-28 18:16:39 +0700

    [IMM] S#18525 - email pos receipt adjustment

---
+++ intercoop_addons/email_pos_receipt/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.fr/>)
+# @author: La Louve
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+
+from . import res_partner
+from . import pos_order
+from . import pos_config_settings
