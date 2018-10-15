14a26c674931fdb81d9413c7155268aa594ce9a1
Nguyen Van Tin <tinnv@trobz.com>
2018-10-15 16:52:06 +0700

    [ADD][F#30485][S#17598]Reporting Modules

--- extra_addons/report_custom_filename/controllers/reports.py
+++ extra_addons/report_custom_filename/controllers/reports.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2014 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-import simplejson
+# Copyright 2014 Therp BV (<http://therp.nl>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import json
 from openerp import http
 from openerp.addons.web.controllers import main
-from openerp.addons.email_template import email_template
+from openerp.addons.mail.models import mail_template
 
 
 class Reports(main.Reports):
@@ -29,19 +13,20 @@ class Reports(main.Reports):
     @main.serialize_exception
     def index(self, action, token):
         result = super(Reports, self).index(action, token)
-        action = simplejson.loads(action)
+        if result.status_code != 200:
+            # In case of error don't change the response.
+            return result
+        action = json.loads(action)
         context = dict(http.request.context)
         context.update(action["context"])
-        report_xml = http.request.session.model('ir.actions.report.xml')
-        report_ids = report_xml.search(
-            [('report_name', '=', action['report_name'])],
-            0, False, False, context)
-        for report in report_xml.browse(report_ids):
-            if not report.download_filename:
-                continue
+        report_xml = http.request.env['ir.actions.report.xml']
+        reports = report_xml.search([
+            ('report_name', '=', action['report_name']),
+            ('download_filename', '!=', False)])
+        for report in reports:
             objects = http.request.session.model(context['active_model'])\
                 .browse(context['active_ids'])
-            generated_filename = email_template.mako_template_env\
+            generated_filename = mail_template.mako_template_env\
                 .from_string(report.download_filename)\
                 .render({
                     'objects': objects,
