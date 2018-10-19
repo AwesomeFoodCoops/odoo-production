39d6c91f515030c3c9919625c1260ed2b621c69e
Nguyen Van Tin <tinnv@trobz.com>
2018-10-19 17:58:53 +0700

    F#30467 - [Workload to Estimate] Email ticket option

--- intercoop_addons/coop_memberspace/models/res_partner.py
+++ intercoop_addons/coop_memberspace/models/res_partner.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from lxml import etree
 from openerp import models, api, fields, _
+from openerp.osv.orm import setup_modifiers
 from openerp.tools.safe_eval import safe_eval
 import logging
 
@@ -117,3 +119,23 @@ class ResPartner(models.Model):
         elif self.sex == 'f':
             return 'Femme'
         return 'Autre'
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form',
+                        context=None, toolbar=False, submenu=False):
+        res = super(ResPartner, self).fields_view_get(
+            view_id=view_id, view_type=view_type, context=context,
+            toolbar=toolbar, submenu=submenu)
+        if view_type == 'form':
+            doc = etree.XML(res['arch'])
+            nodes = doc.xpath("//field[@name='email_pos_receipt']")
+            user = self.env.user
+            has_group_memberspace = user.has_group(
+                'coop_memberspace.group_memberspace')
+            if nodes:
+                if not has_group_memberspace or (
+                        has_group_memberspace and not user.active):
+                    nodes[0].set('invisible', '1')
+                    setup_modifiers(nodes[0], res['fields']['email_pos_receipt'])
+                    res['arch'] = etree.tostring(doc)
+        return res
