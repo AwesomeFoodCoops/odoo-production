de466f26662c0122b7c5ed93270bdd5be068a159
Nguyen Van Tin <tinnv@trobz.com>
2018-10-19 18:15:52 +0700

    [FIX][F#27961][S#16718]Limit date for search in bank reconciliation [P1]

--- intercoop_addons/coop_account/models/__init__.py
+++ intercoop_addons/coop_account/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import account_payment
 from . import account
-from . import account_journal
 from . import account_move
 from . import account_move_line
 from . import account_invoice_line
