64caef7327a84e60fb169a96070fd1a1ccedeb07
Yassine <yteimi01@gmail.com>
2018-11-19 13:30:43 +0100

    [IMP]: Refactoring EDI modules to be generic

--- louve_addons/edi_purchase_base/__openerp__.py
+++ louve_addons/edi_purchase_base/__openerp__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
-# @author: La Louve
+# Copyright (C) 2016-Today: Druidoo (<http://www.druidoo.io/>)
+# @author: Druidoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
 {
-    'name': 'Louve EDI Purchase',
+    'name': 'EDI Purchase',
     'version': '9.0.1.0.2',
     'category': 'Custom',
-    'author': 'La Louve',
+    'author': 'Druidoo',
     'website': 'http://www.lalouve.net',
     'license': 'AGPL-3',
-    'depends': ['purchase', 'louve_edi_config'
+    'depends': ['purchase', 'edi_purchase_config'
     ],
     'data': [
     ],
--- louve_addons/edi_purchase_config/models/edi_mapping_lines.py
+++ louve_addons/edi_purchase_config/models/edi_mapping_lines.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
-# @author: La Louve
+# Copyright (C) 2016-Today: Druidoo (<http://www.druidoo.io/>)
+# @author: Druidoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
 from openerp import models, api, fields
@@ -8,7 +8,7 @@ from openerp import models, api, fields
 
 class EdiMappingLines(models.Model):
     _name = 'edi.mapping.lines'
-    _order ='position'
+    _order = 'position'
 
     config_id = fields.Many2one(comodel_name="edi.config.system")
     sequence = fields.Integer(string="Sequence", required=True, default=1)
