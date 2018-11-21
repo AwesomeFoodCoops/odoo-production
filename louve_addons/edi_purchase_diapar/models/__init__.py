59b242f930deb1125f1bd9f675be38268790ccb9
Jack <lehoangan1988@gmail.com>
2018-11-21 15:41:21 +0700

    [ADD] yassine modules

---
+++ louve_addons/edi_purchase_diapar/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: Druidoo (<http://www.druidoo.io/>)
+# @author: Druidoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+
+from . import product
+from . import edi_config_system
