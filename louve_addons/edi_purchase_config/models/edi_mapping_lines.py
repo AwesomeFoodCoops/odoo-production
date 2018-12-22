b225f162ca0b9088b38c0d2cceb0b05ec833b5a8
Yassine <yteimi01@gmail.com>
2018-12-23 00:22:54 +0100

    [IMP]: Prices update EDI interface feature

--- louve_addons/edi_purchase_config/models/edi_mapping_lines.py
+++ louve_addons/edi_purchase_config/models/edi_mapping_lines.py
@@ -12,6 +12,23 @@ class EdiMappingLines(models.Model):
 
     config_id = fields.Many2one(comodel_name="edi.config.system")
     sequence = fields.Integer(string="Sequence", required=True, default=1)
+    position = fields.Integer(string="Position", required=True)
     value = fields.Char(string="Python code", help="Use python code to generate your order file, using methods defined"
                                                    "on the configuration system model", required=True)
+
+
+class EdiPriceMapping(models.Model):
+    _name = 'edi.price.mapping'
+    _order = 'position'
+
+    sequence = fields.Integer(string="Sequence", default=1)
     position = fields.Integer(string="Position", required=True)
+    price_config_id = fields.Many2one(comodel_name="edi.config.system")
+    mapping_field_id = fields.Many2one(comodel_name="ir.model.fields",
+                                       domain="[('model', '=', 'supplier.price.list')]", string="Prices mapping field")
+    name = fields.Char(string="Zone description")
+    sequence_start = fields.Integer(string="Sequence start")
+    sequence_end = fields.Integer(string="Sequence end")
+    is_numeric = fields.Boolean(string="Is numeric ?")
+    is_date = fields.Boolean(string="Is a date?")
+    decimal_precision = fields.Integer(string="Decimal precision")
