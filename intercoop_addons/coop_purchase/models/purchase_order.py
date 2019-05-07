ec761d0fd5ec27f0e730a00d0a882390b294126b
Phat Nguyen <phatnt@trobz.com>
2019-05-07 10:55:14 +0700

    [FIX] Fix delete canceled purchase orders feature

--- intercoop_addons/coop_purchase/models/purchase_order.py
+++ intercoop_addons/coop_purchase/models/purchase_order.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from openerp import api, models
+from openerp import api, fields, models
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    # Make it required only in view to delete purchase orders
+    date_planned = fields.Datetime(
+        string='Scheduled Date',
+        compute='_compute_date_planned',
+        required=False,
+        store=True,
+        index=True,
+        oldname='minimum_planned_date'
+    )
 
     @api.multi
     def button_update_prices(self):
