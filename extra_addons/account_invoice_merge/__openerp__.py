21f9a93487118cbd8a5b182033113466156483b2
JackLe <anlh@trobz.com>
2019-05-27 12:37:06 +0700

    [ADD] module merge invoice

---
+++ extra_addons/account_invoice_merge/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2010-2011 Elico Corp. All Rights Reserved.
+# Author: Ian Li <ian.li@elico-corp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Invoice Merge Wizard',
+    'version': '9.0.1.0.2',
+    'category': 'Finance',
+    'author': "Elico Corp, Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/account-invoicing/',
+    'license': 'AGPL-3',
+    'depends': ['account'],
+    'data': [
+        'wizard/invoice_merge_view.xml',
+    ],
+    'installable': True,
+}
