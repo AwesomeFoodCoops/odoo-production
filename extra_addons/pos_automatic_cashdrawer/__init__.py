db580f781011527e870bf9f46ddc18a756afdfd6
Iván Todorovich <ivan.todorovich@druidoo.io>
2019-10-16 14:03:41 +0000

    [REF] Refactor - hw_cashlogy: Refactor connection
    - pos_automatic_cashdrawer module structure. Removed unused code. Aligned to OCA guidelines
    
    - Renamed hw_cashlogy to hw_automatic_cashdrawer_cashlogy
    
    - Refactor transaction start
    
    - Smart-transform send parameters
    
    - Add get total amount

--- extra_addons/pos_automatic_cashdrawer/__init__.py
+++ extra_addons/pos_automatic_cashdrawer/__init__.py
@@ -1,3 +1,1 @@
-from . import pos_automatic_cashdrawer
-from . import account_journal
 from . import models
--- extra_addons/pos_automatic_cashdrawer/models/__init__.py
+++ extra_addons/pos_automatic_cashdrawer/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 from . import pos_config
-from . import pos_session
+from . import account_journal
