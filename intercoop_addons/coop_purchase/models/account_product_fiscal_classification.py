e15feb7c99faa4f83fe6479f58364d05ce3686b1
Iván Todorovich <ivan.todorovich@druidoo.io>
2019-11-05 18:26:53 +0000

    [IMP] Allow to modify accounts on products, if the account is not present on the fiscal classification

--- intercoop_addons/coop_purchase/models/account_product_fiscal_classification.py
+++ intercoop_addons/coop_purchase/models/account_product_fiscal_classification.py
@@ -5,25 +5,18 @@ from openerp import models, fields
 class AccountProductFiscalClassification(models.Model):
     _inherit = 'account.product.fiscal.classification'
 
+    # TODO: this should be company_dependant
     income_account_id = fields.Many2one(
-        comodel_name='account.account',
+        'account.account',
         string='Income Account',
-        required=True
     )
+
+    # TODO: this should be company_dependant
     expense_account_id = fields.Many2one(
-        comodel_name='account.account',
+        'account.account',
         string='Expense Account',
-        required=True
     )
-    sale_tax_ids = fields.Many2many(
-        comodel_name='account.tax',
-        relation='fiscal_classification_sale_tax_rel',
-        column1='fiscal_classification_id', column2='tax_id',
-        required=True
-    )
-    purchase_tax_ids = fields.Many2many(
-        comodel_name='account.tax',
-        relation='fiscal_classification_purchase_tax_rel',
-        column1='fiscal_classification_id', column2='tax_id',
-        required=True
-    )
+
+    # Make tax required
+    sale_tax_ids = fields.Many2many(required=True)
+    purchase_tax_ids = fields.Many2many(required=True)
