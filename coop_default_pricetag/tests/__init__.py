f7a672e17d24660fc8461154bcdb2efd4a0873e6
Chetan <scrdhaduk@gmail.com>
2020-02-12 11:02:15 +0530

    [Add]add test case, make proper licence term.

--- coop_default_pricetag/tests/__init__.py
+++ coop_default_pricetag/tests/__init__.py
@@ -4,4 +4,4 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import product_print_wizard
+from . import test_product_print_category
---
+++ coop_default_pricetag/tests/test_product_print_category.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2012-Today GRAP (http://www.grap.coop)
+# Copyright (C) 2020-Today: Druidoo (<https://www.druidoo.io>)
+# @author Julien WESTE
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestProductPrintCategory(TransactionCase):
+    """Tests for 'Product Print Category' Module"""
+
+    def setUp(self):
+        super(TestProductPrintCategory, self).setUp()
+        self.wizard_obj = self.env["product.print.wizard"]
+        # self.report_obj = self.env["report"]
+        self.custom_report_obj = self.env[
+            "report.product_print_category.report_pricetag"]
+        self.print_category = self.env.ref(
+            "product_print_category.demo_category")
+
+    # Test Section
+    def test_01_test_wizard_obsolete(self):
+        wizard = self.wizard_obj.with_context(
+            active_model="product.print.category",
+            active_ids=[self.print_category.id]).create({})
+        self.assertEqual(
+            len(wizard.line_ids),
+            1,
+            'Print obsolete product should propose 1 product'
+        )
+
+    def test_02_test_wizard_all(self):
+        wizard = self.wizard_obj.with_context(
+            active_model="product.print.category",
+            active_ids=[self.print_category.id], all_products=True, ).create({
+            })
+        self.assertEqual(
+            len(wizard.line_ids),
+            2,
+            "Print all products should propose 5 products"
+        )
+        data = wizard.print_report()
+        self.env['report.coop_default_pricetag.report_pricetag'].\
+            _get_report_values(docids=None, data=data.get('data'))
+        self.env['report.coop_default_pricetag.report_pricetag_barcode'].\
+            _get_report_values(docids=None, data=data.get('data'))
