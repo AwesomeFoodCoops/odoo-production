190d342f43b525147d6d39ebef946c38f267d464
Chetan <scrdhaduk@gmail.com>
2020-02-19 16:56:45 +0530

    [MIG]Migration to 12.0: coop_productt_coefficient

---
+++ coop_product_coefficient/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import product_product
+from . import product_template
+from . import product_coefficient
+from . import wizard_use_theoritical_price
+from . import sale_config_settings
+from . import product_category
