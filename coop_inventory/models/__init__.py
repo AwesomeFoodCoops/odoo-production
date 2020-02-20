494414e081eeed5ef11630db38ebe1178cff3a84
Krunal <krunal267@gmail.com>
2020-02-20 21:15:30 +0530

    [MIG] Migrated to 12.0: coop_inventory

--- coop_inventory/models/__init__.py
+++ coop_inventory/models/__init__.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
 # @author: La Louve
+# Copyright (C) 2020-Today: Druidoo (<https://www.druidoo.io>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
 from . import purchase_order
-from . import stock_inventory
 from . import stock_picking
-from . import stock_quant
