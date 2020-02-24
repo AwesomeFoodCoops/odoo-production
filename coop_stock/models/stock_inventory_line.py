95234eb5af4bee9b510b6de667d9c6b5ddb4e4ff
Chetan <scrdhaduk@gmail.com>
2020-02-24 19:04:46 +0530

    Rermove unuserful api keywrod from file.

--- coop_stock/models/stock_inventory_line.py
+++ coop_stock/models/stock_inventory_line.py
@@ -2,7 +2,7 @@
 # @author: La Louve
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
-from odoo import api, models
+from odoo import models
 
 
 class StockInventoryLine(models.Model):
