0ee84cf7898a0cab4d093c1403b553c724ad8873
Ajay Javiya <javiyaajay1987@gmail.com>
2020-02-25 10:29:43 +0530

    [MIG] Migration to 12.0: coop_account

---
+++ coop_account/wizard/__init__.py
@@ -0,0 +1,4 @@
+
+from . import bank_statement_line_reconcile_wizard
+from . import account_move_reversal
+from . import unmatch_bank_statement_wizard
--- intercoop_addons/coop_account/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_payment
-from . import account
-from . import account_move
-from . import account_move_line
-from . import account_invoice_line
-from . import account_bank_statement_line
-from . import account_invoice
-from . import account_asset_asset
