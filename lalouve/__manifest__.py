806598c401f12feabf9cc3483e0dc30d581d5d64
Iván Todorovich <ivan.todorovich@druidoo.io>
2020-02-28 15:16:36 +0100

    Update __manifest__.py

--- lalouve/__manifest__.py
+++ lalouve/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
 {
-    "name": "Louve Custom - Email",
+    "name": "La Louve Customizations",
     "version": "12.0.1.0.0",
     "category": "Custom",
     "author": "La Louve",
