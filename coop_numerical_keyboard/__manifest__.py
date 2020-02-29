bc34bc8f0849c7fb52380a9cbe0b6172b490e5d7
Ajay Patel <dev.ipredictitsolutions@gmail.com>
2020-02-29 11:06:41 +0530

    [IMP] change manifest version and fix code structure

--- coop_numerical_keyboard/__manifest__.py
+++ coop_numerical_keyboard/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-Today: La Louve (<https://cooplalouve.fr>)
 # @author: Simon Mas (linkedin.com/in/simon-mas)
+# Copyright (C) 2019-Today: Druidoo (<https://www.druidoo.io>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
 {
     'name': 'Coop Numerical Keyboard',
-    'version': '9.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Tools',
     'summary': 'Create a new POS numerical keyboard',
     'author': 'Simon Mas, Phat Nguyen Tan, Odoo Community Association (OCA)',
