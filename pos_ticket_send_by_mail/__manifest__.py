marcelsavegnago/FoodCoops

[MIG]Migration to 12.0: email_pos_receipt

email_pos_receipt/__manifest__.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: La Louve (<http://www.lalouve.fr/>)
+# Copyright (C) 2019-Today: Druidoo (<https://www.druidoo.io>)
 # @author: La Louve
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
@@ -10,6 +10,8 @@
     'description': """
         Send Receipt By Email
     """,
+    'author': 'Druidoo',
+   'website': 'https://www.druidoo.io',
 
     'license': 'AGPL-3',
     'depends': [
@@ -22,10 +24,11 @@
         'data/ir_cron_data.xml',
 
         # view
-        'views/view_pos_config_settings.xml',
+        'views/view_res_config_settings.xml',
         'views/view_res_partner.xml',
 
         # templates
         'static/src/xml/templates.xml',
     ],
+    'installable': True,
 }

AwesomeFoodCoops/odoo-production

pos_ticket_send_by_mail/__manifest__.py
@@ -1,31 +1,27 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: La Louve (<http://www.lalouve.fr/>)
+# Copyright (C) 2019-Today: Druidoo (<https://www.druidoo.io>)
 # @author: La Louve
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
 {
     'name': 'POS Receipt By Email',
-    'version': '9.0.1.0.11',
+    'version': '12.0.1.0.0',
     'category': 'Custom',
-    'description': """
-        Send Receipt By Email
-    """,
-
+    'author': 'Druidoo',
+    'website': 'https://www.druidoo.io',
     'license': 'AGPL-3',
     'depends': [
         'point_of_sale',
     ],
     'data': [
         'security/ir.model.access.csv',
-        # datas
+        'views/report_paperformat.xml',
+        'views/view_pos_config_settings.xml',
         'data/email_template_data.xml',
         'data/ir_cron_data.xml',
-
-        # view
-        'views/view_pos_config_settings.xml',
         'views/view_res_partner.xml',
-
-        # templates
+        'views/report_receipt.xml',
         'static/src/xml/templates.xml',
     ],
+    'installable': True,
 }