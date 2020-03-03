1507ca8fc30edd6f0250dc6a7b4476cdde3d2294
Chetan <scrdhaduk@gmail.com>
2020-03-03 21:07:40 +0530

    Remove unnecessary api keyword

--- edi_purchase_config/models/edi_mapping_lines.py
+++ edi_purchase_config/models/edi_mapping_lines.py
@@ -2,7 +2,7 @@
 # @author: Druidoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
-from odoo import models, api, fields
+from odoo import models, fields
 
 
 class EdiMappingLines(models.Model):
