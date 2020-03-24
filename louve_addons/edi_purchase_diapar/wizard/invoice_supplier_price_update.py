abddcbd5f148fb27a37e77cf5731a4f4a709bcd7
Chetan <scrdhaduk@gmail.com>
2020-03-24 16:11:58 +0530

    DIAPAR Improvements

--- louve_addons/edi_purchase_diapar/wizard/invoice_supplier_price_update.py
+++ louve_addons/edi_purchase_diapar/wizard/invoice_supplier_price_update.py
@@ -6,9 +6,52 @@
 from openerp import api, fields, models, _
 from openerp.exceptions import ValidationError
 
+supported_models = [
+    'account.invoice',
+]
 
-class SupplierInfoUpdate(models.TransientModel):
-    _inherit = 'supplier.info.update'
+class InvoiceSupplierPriceUpdate(models.TransientModel):
+    _name = 'invoice.supplier.price.update'
+
+    show_discount = fields.Boolean()
+    partner_id = fields.Many2one(comodel_name='res.partner')
+    edi_line_ids = fields.One2many(
+        comodel_name='supplier.info.update.line',
+        inverse_name='inv_supplier_price_id'
+    )
+    edi_line2_ids = fields.One2many(
+        comodel_name='supplier.info.update.line',
+        inverse_name='inv_supplier_price_id'
+    )
+
+    @api.onchange('show_discount')
+    def _onchange_show_discount(self):
+        self.ensure_one()
+        if self.show_discount:
+            self.edi_line_ids = self.edi_line2_ids
+            self.edi_line2_ids = self.env['supplier.info.update.line']
+        else:
+            self.edi_line2_ids = self.edi_line_ids
+            self.edi_line_ids = self.env['supplier.info.update.line']
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(InvoiceSupplierPriceUpdate, self).default_get(
+            fields_list=fields_list)
+        active_model = self._context.get('active_model', '')
+        active_id = self._context.get('active_id', False)
+        if active_model in supported_models and active_id:
+            active_obj = self.env[active_model].browse(active_id)
+            processed_lines = self.compute_process_lines(
+                active_model, active_obj)
+            show_discount = active_obj.partner_id.show_discount
+            res.update({
+                'edi_line_ids': processed_lines,
+                'edi_line_ids': processed_lines,
+                'partner_id': active_obj.partner_id.id,
+                'show_discount': show_discount,
+            })
+        return res
 
     @api.model
     def compute_edi_partner(self, partner_id):
@@ -52,6 +95,14 @@ class SupplierInfoUpdate(models.TransientModel):
             :param supplier: Supplier.
             :return: Updated lines.
         """
+        # For invoice date
+        active_model = self._context.get('active_model', '')
+        active_id = self._context.get('active_id', False)
+        invoice_id = self.env['account.invoice'].browse(active_id)
+        invoice_date = False
+        if invoice_id:
+            invoice_date = invoice_id[0].date_invoice
+        # END
         # Only for EDI suppliers
         if supplier.is_edi:
             # Clear old list elements
@@ -73,9 +124,12 @@ class SupplierInfoUpdate(models.TransientModel):
                                             'supplier code to continue prices operation update') % (product.name,
                                                                                                     supplier.name))
                 # Look for EDI latest price
-                edi_price = self.env['supplier.price.list'].search([('supplier_id', '=', edi_supplier.id),
-                                                                    ('supplier_code', '=', product_code)],
-                                                                   order="import_date DESC")
+                price_domain = []
+                price_domain.append(('supplier_id', '=', edi_supplier.id))
+                price_domain.append(('supplier_code', '=', product_code))
+                if invoice_date:
+                    price_domain.append(('apply_date', '>=', invoice_date))
+                edi_price = self.env['supplier.price.list'].search(price_domain, order="import_date DESC")
                 if edi_price:
                     price = edi_price[0]
                     price_dict = {'price_unit': price.price}
@@ -97,8 +151,37 @@ class SupplierInfoUpdate(models.TransientModel):
         :param active_obj: Current active object
         :return: computed lines
         """
-        lines = super(SupplierInfoUpdate, self).compute_process_lines(active_model, active_obj)
+        lines = self.env['supplier.info.update'].compute_process_lines(active_model, active_obj)
         seller_values_lines = [line[2] for line in lines]
         partner_id = active_obj.partner_id
         edi_supplier = self.compute_edi_partner(partner_id)
         return self.update_prices_edi(lines, active_model, seller_values_lines, partner_id, edi_supplier)
+
+    @api.multi
+    def update_prices_second(self):
+        self.ensure_one()
+        lines = self.edi_line_ids.sorted()
+        for line in lines:
+            update_values = {}
+            supplier_price_unit = line.supplier_price_unit
+            updated_price_unit = line.price_unit
+            if updated_price_unit > 0 and updated_price_unit != supplier_price_unit:
+                update_values['base_price'] = updated_price_unit
+
+            supplier_discount = line.supplier_discount
+            updated_discount = line.observable_discount
+            if updated_discount != supplier_discount:
+                update_values['discount'] = updated_discount
+            if update_values:
+                seller_id = line.seller_id
+                product_template = seller_id.product_tmpl_id
+
+                seller_id.write(update_values)
+                product_template.auto_update_theoritical_cost_price()
+        return True
+
+
+class SupplierInfoUpdateLine(models.TransientModel):
+    _inherit = 'supplier.info.update.line'
+
+    inv_supplier_price_id = fields.Many2one(comodel_name='invoice.supplier.price.update')
