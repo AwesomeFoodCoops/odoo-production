aa199f059ca7952ded7208eef2735d351d9d3f61
Ajay Javiya <javiyaajay1987@gmail.com>
2020-04-01 14:14:25 +0530

    [MIG] Migration to 12.0: account_payment_select_account

--- account_payment_select_account/__manifest__.py
+++ account_payment_select_account/__manifest__.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
+# Copyright (C) 2019-Today: Druidoo (<https://www.druidoo.io>)
 # @author: La Louve
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 
 {
     'name': 'Account Payment - Select Account',
-    'version': '9.0.7.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'description': '''
         Allow user to select the account on Register Payment of Invoice if
         the default Debit / Credit account is not set in the selected Journal
     ''',
-    'author': 'La Louve',
+    'author': 'La Louve, Druidoo',
     'website': 'http://www.lalouve.net',
     'license': 'AGPL-3',
     'depends': [
