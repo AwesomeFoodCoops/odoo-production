c7000cdefc1cb4ea9a9eb53b9b34674cb372a555
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-04-21 14:21:35 +0200

    [REM] Unused code

--- coop_account/models/account_payment.py
+++ coop_account/models/account_payment.py
@@ -5,8 +5,6 @@ from odoo import api, models, fields, _
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    account_move_ids = fields.One2many(
-        'account.move', 'payment_id', 'Journals Entry')
     state = fields.Selection(selection_add=[('cancelled', 'Cancelled')])
     partner_code = fields.Integer(
         related='partner_id.barcode_base', store=True)
@@ -61,39 +59,3 @@ class AccountPayment(models.Model):
             lcr_nb = _('LCR nb ')
             self.communication = '%s-%s%s' %\
                 (communication, lcr_nb, self.text_lcr_code)
-
-    @api.multi
-    def reverse_payments(self):
-        self.ensure_one()
-        ctx = dict(self._context)
-        ctx.update({
-            'active_ids': self.account_move_ids.ids,
-            'payment_id': self.id,
-        })
-        return {
-            'name': _('Reverse Moves'),
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'account.move.reversal',
-            'view_id': self.env.ref(
-                'account.view_account_move_reversal').id,
-            'type': 'ir.actions.act_window',
-            'target': 'new',
-            'context': ctx,
-        }
-
-    @api.multi
-    def re_generate(self, default=None):
-        self.ensure_one()
-        new_record = super(AccountPayment, self).copy(default=default)
-        new_record.invoice_ids = self.invoice_ids
-        return {
-            'name': _('Draft Payment'),
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'account.payment',
-            'view_id': self.env.ref(
-                'AccountPayment_transfer_account.view_AccountPayment_form_extend').id,
-            'type': 'ir.actions.act_window',
-            'res_id': new_record.id
-        }
