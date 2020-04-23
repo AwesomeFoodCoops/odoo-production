d33eb82be729695ef51a4853d765986a8537da51
siddharth <siddharth>
2020-04-23 18:39:21 +0530

    [ADD]  Contactless payments

--- intercoop_addons/account_reconcile_pos_payments/models/account_bank_statement.py
+++ intercoop_addons/account_reconcile_pos_payments/models/account_bank_statement.py
@@ -80,91 +80,146 @@ class AccountBankStatement(models.Model):
                     'The journal %s has no CB Contract number.'
                     ) % rec.journal_id.display_name)
 
+            if rec.journal_id.cb_contactless_matching and not rec.journal_id.cb_contract_number_contactless:
+                raise UserError(_(
+                    'The journal %s has no CT Contract number.'
+                ) % rec.journal_id.display_name)
             # Process lines that match the pattern
-            line_ids = rec.line_ids.search([
+            cb_line_ids = rec.line_ids.search([
                 ('statement_id', '=', rec.id),
                 ('journal_entry_ids', '=', False),
                 ('name', 'ilike', '%%%s%%' % rec.journal_id.cb_contract_number),
             ])
-
-            i = 0
-            for line in line_ids:
-                i += 1
-                _logger.debug('Processing line %s (%d/%d)' % (line.name, i, len(line_ids)))
-
-                # Try to find a CB statement older than our statement line
-                # but not older than 3 days before
-                limit_date = (
-                    datetime.strptime(line.date, '%Y-%m-%d')
-                    - timedelta(days=rec.journal_id.cb_delta_days))
-                cb_statement_ids = self.env['account.bank.statement'].search([
-                    ('date', '<=', line.date),
-                    ('date', '>=', limit_date.strftime('%Y-%m-%d')),
-                    ('balance_end_real', '>', round(line.amount-0.01, 2)),
-                    ('balance_end_real', '<', round(line.amount+0.01, 2)),
-                    ('line_ids', '!=', False),
+            # Process CB Statement lines first
+            not_reconcile_cb_lines = rec._get_correct_cb_statement_id(cb_line_ids)
+            # Process CT Statement lines first
+            not_reconcile_ct_lines = self.env['account.bank.statement.line']
+            if rec.journal_id.cb_contactless_matching:
+                ct_line_ids = rec.line_ids.search([
+                    ('statement_id', '=', rec.id),
+                    ('journal_entry_ids', '=', False),
+                    ('name', 'ilike', '%%%s%%' % rec.journal_id.cb_contract_number_contactless),
                 ])
+                not_reconcile_ct_lines = rec._get_correct_ct_statement_id(ct_line_ids)
+            # Process CB+CT Statement lines
+            if not_reconcile_cb_lines and not_reconcile_ct_lines:
+                rec._get_correct_cb_ct_statement_id(not_reconcile_cb_lines, not_reconcile_ct_lines)
 
-                _logger.debug('Possible CB statements: %d' % len(cb_statement_ids))
+    def _get_correct_cb_statement_id(self, cb_line_ids):
+        not_reconcile_lines = self.env['account.bank.statement.line']
+        for line in cb_line_ids:
+            delta_days = self.journal_id.cb_delta_days
+            rounding = self.journal_id.cb_rounding
+            res = self.get_correct_pos_statement(line.date, line.amount,delta_days,rounding)
+            if not res or len(res) > 1:
+                _logger.debug('Unable to match line or multiple possible statements for "%s" line' % line.name)
+                not_reconcile_lines |= line
+                continue
+            self.process_statement_reconcile(line, res)
+        return not_reconcile_lines
 
-                # Only accept statements whose account movements are fully unreconciled
-                ignore_cb_statement_ids = []
-                for cb_statement_id in cb_statement_ids:
-                    reconciled_move_lines_count = \
-                        len(cb_statement_id.move_line_ids.filtered(lambda l:
-                            l.reconciled and l.account_id.id == \
-                                rec.journal_id.default_debit_account_id.id))
-                    if reconciled_move_lines_count:
-                        if len(cb_statement_id) != reconciled_move_lines_count:
-                            _logger.warning('Level 2: POS partially processed by the bank.')
-                        else:
-                            _logger.debug('Case 1: There are debits on the journal and they are reconciled.')
-                        # In any case, don't use this statement
-                        ignore_cb_statement_ids.append(cb_statement_id.id)
-                cb_statement_ids = cb_statement_ids.filtered(lambda s: s.id not in ignore_cb_statement_ids)
-
-                if not cb_statement_ids:
-                    _logger.debug('Unable to match line "%s" to a CB statement' % line.name)
+    def _get_correct_ct_statement_id(self, ct_line_ids):
+        not_reconcile_lines = self.env['account.bank.statement.line']
+        if ct_line_ids:
+            for line in ct_line_ids:
+                delta_days = self.journal_id.cb_contactless_delta_days
+                rounding = self.journal_id.cb_rounding
+                res = self.get_correct_pos_statement(line.date, line.amount, delta_days, rounding)
+                if not res or len(res) > 1:
+                    _logger.debug('Unable to match line or multiple possible statements for "%s" line' % line.name)
+                    not_reconcile_lines |= line
                     continue
+                self.process_statement_reconcile(line, res)
+        return not_reconcile_lines
 
-                if len(cb_statement_ids)>1:
-                    _logger.warning('There are multiple possible statements for this line. Unable to process. %s' % line)
+    def _get_correct_cb_ct_statement_id(self, not_reconcile_cb_lines, not_reconcile_ct_lines):
+        for cb_line in not_reconcile_cb_lines:
+            for ct_line in not_reconcile_ct_lines:
+                total_amount = cb_line.amount + ct_line.amount
+                delta_days = self.journal_id.cb_delta_days
+                ct_delta_days = self.journal_id.cb_contactless_delta_days
+                rounding = self.journal_id.cb_rounding
+                res = self.get_correct_pos_statement(cb_line.date, total_amount, delta_days, rounding, ct_date=ct_line.date, ct_delta_days=ct_delta_days)
+                if not res or len(res) > 1:
+                    _logger.debug('Unable to match line or multiple possible statements for "%s" and "%s" line' % (cb_line.name, ct_line.name))
                     continue
+                self.process_statement_reconcile(cb_line,res, ct_line=ct_line)
 
-                # If we got this far, means we have a match!
-                # Let's create the move line in our journal
-                move_line_vals = {
-                    'name': '%s %s %s %s' % (
-                        rec.journal_id.cb_contract_number,
-                        cb_statement_id.journal_id.name,
-                        cb_statement_id.date,
-                        cb_statement_id.name),
-                    'debit': 0.0,
-                    'credit': abs(line.amount),
-                    'journal_id': rec.journal_id.id,
-                    'date': cb_statement_id.date,
-                    'account_id': cb_statement_id.journal_id.default_credit_account_id.id,
-                }
+    def get_correct_pos_statement(self, line_date, amount, delta_days, rounding, ct_date=False,ct_delta_days=0):
+        limit_date = (
+                datetime.strptime(line_date, '%Y-%m-%d')
+                - timedelta(days=delta_days))
+        pos_statement_ids = self.env['account.bank.statement'].search([
+            ('date', '<=', line_date),
+            ('date', '>=', limit_date.strftime('%Y-%m-%d')),
+            ('balance_end_real', '>=', round(amount - rounding, 2)),
+            ('balance_end_real', '<=', round(amount + rounding, 2)),
+            ('line_ids', '!=', False),
+        ])
+        if not pos_statement_ids and ct_date:
+            ct_limit_date = (
+                    datetime.strptime(ct_date, '%Y-%m-%d')
+                    - timedelta(days=ct_delta_days))
+            pos_statement_ids = self.env['account.bank.statement'].search([
+                ('date', '<=', ct_date),
+                ('date', '>=', ct_limit_date.strftime('%Y-%m-%d')),
+                ('balance_end_real', '>', round(amount - rounding, 2)),
+                ('balance_end_real', '<', round(amount + rounding, 2)),
+                ('line_ids', '!=', False),
+            ])
 
-                _logger.debug('Creating reconciliation line: %s' % move_line_vals)
-                line.process_reconciliation([], [], [move_line_vals])
+        _logger.debug('Possible CB/CT/CB+CT statements: %d' % len(pos_statement_ids))
+        ignore_cb_statement_ids = []
+        for pos_statement_id in pos_statement_ids:
+            reconciled_move_lines_count = \
+                len(pos_statement_id.move_line_ids.filtered(lambda l:
+                                                           l.reconciled and l.account_id.id == \
+                                                           self.journal_id.default_debit_account_id.id))
+            if reconciled_move_lines_count:
+                if len(pos_statement_id) != reconciled_move_lines_count:
+                    _logger.debug('Level 2: POS partially processed by the bank.')
+                else:
+                    _logger.debug('Case 1: There are debits on the journal and they are reconciled.')
+                # In any case, don't use this statement
+                ignore_cb_statement_ids.append(pos_statement_id.id)
+        pos_statement_ids = pos_statement_ids.filtered(lambda s: s.id not in ignore_cb_statement_ids)
+        return pos_statement_ids
 
-                # Now let's settle this line with the statement lines
-                lines_to_reconcile = []
+    def process_statement_reconcile(self, lines, statement_id, ct_line=False):
+        if ct_line:
+            lines |= ct_line
+        for line in lines:
+            move_line_vals = {
+                'name': '%s %s %s %s' % (
+                    self.journal_id.cb_contract_number,
+                    statement_id.journal_id.name,
+                    statement_id.date,
+                    statement_id.name),
+                'debit': 0.0,
+                'credit': abs(line.amount),
+                'journal_id': self.journal_id.id,
+                'date': statement_id.date,
+                'account_id': statement_id.journal_id.default_credit_account_id.id,
+            }
 
-                for move_line in cb_statement_id.move_line_ids:
-                    if (
-                        move_line.account_id.id == cb_statement_id.journal_id.default_debit_account_id.id
+            _logger.debug('Creating reconciliation line: %s' % move_line_vals)
+            line.process_reconciliation([], [], [move_line_vals])
+            # Now let's settle this line with the statement lines
+            lines_to_reconcile = []
+
+            for move_line in statement_id.move_line_ids:
+                if (
+                        move_line.account_id.id == statement_id.journal_id.default_debit_account_id.id
                         and move_line.id not in lines_to_reconcile
-                    ):
-                        lines_to_reconcile.append(move_line.id)
+                ):
+                    lines_to_reconcile.append(move_line.id)
 
-                for move_line in line.journal_entry_ids.mapped('line_ids'):
-                    if (
-                        move_line.account_id.id == cb_statement_id.journal_id.default_credit_account_id.id
+            for move_line in line.journal_entry_ids.mapped('line_ids'):
+                if (
+                        move_line.account_id.id == statement_id.journal_id.default_credit_account_id.id
                         and move_line.id not in lines_to_reconcile
-                    ):
-                        lines_to_reconcile.append(move_line.id)
+                ):
+                    lines_to_reconcile.append(move_line.id)
 
-                # TODO: Also reconcile contactless lines here
-                self.env['account.move.line.reconcile'].with_context(active_ids=lines_to_reconcile).create({}).trans_rec_reconcile_full()
+            self.env['account.move.line.reconcile'].with_context(active_ids=lines_to_reconcile).create(
+                {}).trans_rec_reconcile_full()
