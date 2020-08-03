b2892f870858af25efc23eab59b9a71ff48d3b9b
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-08-03 18:00:42 +0200

    [FIX] Avoid constraint in capital subscription

--- capital_subscription/models/account_full_reconcile.py
+++ capital_subscription/models/account_full_reconcile.py
@@ -9,7 +9,6 @@ from odoo.exceptions import UserError
 class AccountFullReconcile(models.Model):
     _inherit = 'account.full.reconcile'
 
-    # Custom Section
     @api.multi
     def generate_capital_entrie(self, undo=False):
         move_obj = self.env['account.move']
@@ -17,14 +16,11 @@ class AccountFullReconcile(models.Model):
             category_ids = reconcile.mapped(
                 'reconciled_line_ids.invoice_id.fundraising_category_id')
             invoices = reconcile.mapped('reconciled_line_ids.invoice_id')
-
             # get payment entry
             payment_id = reconcile.mapped('reconciled_line_ids.payment_id')
-
             # get line to reconcile
             move_line = invoices.mapped('move_id.line_ids')
             line_to_reconcile = move_line.filtered(lambda l: l.credit != 0)
-
             # Do not create any extra entry for capital refund
             if not invoices or invoices[0].type == 'out_refund':
                 continue
@@ -32,15 +28,16 @@ class AccountFullReconcile(models.Model):
             if len(category_ids) > 1:
                 raise UserError(_(
                     "You can not reconcile Capital Invoices for many"
-                    " Categories: %s" % (
-                        ', '.join(category_ids.mapped('name')))))
+                    " Categories: %s") % (
+                        ', '.join(category_ids.mapped('name'))))
             elif len(category_ids) == 1 and category_ids[0].capital_account_id:
                 # Create new account move
                 category = category_ids[0]
-                capital_entries_journal = \
-                    category.fundraising_id.journal_ids and \
-                    category.fundraising_id.journal_ids or \
-                    category.fundraising_id.journal_id
+                capital_entries_journal = (
+                    category.fundraising_id.journal_ids
+                    and category.fundraising_id.journal_ids
+                    or category.fundraising_id.journal_id
+                )
                 partner = reconcile.reconciled_line_ids[0].partner_id
 
                 move_lines = reconcile.mapped('reconciled_line_ids').filtered(
@@ -51,12 +48,17 @@ class AccountFullReconcile(models.Model):
                 journal = move_lines[0].journal_id
                 payment_move_lines = invoices[0].payment_move_line_ids
 
-                payment_m_line = \
-                    reconcile.mapped('reconciled_line_ids').filtered(
-                        lambda ml: ml.id in payment_move_lines.ids
-                    )
-                payment_date = payment_m_line and payment_m_line[0].date or \
-                    fields.Date.context_today(self)
+                payment_m_line = reconcile.mapped(
+                    'reconciled_line_ids'
+                ).filtered(
+                    lambda ml: ml.id in payment_move_lines.ids
+                )
+
+                payment_date = (
+                    payment_m_line
+                    and payment_m_line[0].date
+                    or fields.Date.context_today(self)
+                )
 
                 # Sale
                 total = sum(move_lines.mapped('debit'))
@@ -71,7 +73,6 @@ class AccountFullReconcile(models.Model):
                     'invoice_id': invoices[0].id,
                     'debit': total if is_payment else 0,
                     'credit': 0 if is_payment else total,
-                    'payment_id': payment_id and payment_id[0].id or False
                 }), (0, 0, {
                     'name': _("Payment of Capital"),
                     'partner_id': partner.id,
@@ -80,7 +81,6 @@ class AccountFullReconcile(models.Model):
                     'invoice_id': invoices[0].id,
                     'debit': 0 if is_payment else total,
                     'credit': total if is_payment else 0,
-                    'payment_id': payment_id and payment_id[0].id or False
                 })]
 
                 move_vals = {
@@ -92,9 +92,15 @@ class AccountFullReconcile(models.Model):
                     'date': payment_date,
                     'narration': _("Paid Capital"),
                 }
+
                 move = move_obj.create(move_vals)
                 move.post()
-
+                # add payment link
+                # we do it after to avoid a constraint
+                if payment_id:
+                    move.mapped('line_ids').write({
+                        'payment_id': payment_id[0].id,
+                    })
                 # auto reconcile
                 if line_to_reconcile and not line_to_reconcile[0].reconciled:
                     reconcile.auto_reconcile_payment(
