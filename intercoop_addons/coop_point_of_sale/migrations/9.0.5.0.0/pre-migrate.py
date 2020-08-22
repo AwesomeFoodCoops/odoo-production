cb1141973fc4b92f9d4fa541f4acf178be9b00df
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-08-22 16:18:45 +0200

    [FIX] coop_point_of_sale mig script

--- intercoop_addons/coop_point_of_sale/migrations/9.0.5.0.0/pre-migrate.py
+++ intercoop_addons/coop_point_of_sale/migrations/9.0.5.0.0/pre-migrate.py
@@ -10,7 +10,7 @@ def migrate(cr, version):
         WHERE table_name = 'pos_order'
         AND column_name = 'week_name'
     """)
-    if env.cr.fetchall():
+    if cr.fetchall():
         return
 
     # Avoids recomputation of new field week_name
@@ -32,31 +32,21 @@ def migrate(cr, version):
     # populates week number
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        get_param = env['ir.config_parameter'].sudo().get_param
-        weekA_date = get_param('coop_shift.week_a_date')
-        n_weeks_cycle = int(get_param('coop_shift.number_of_weeks_per_cycle'))
-
-        # helper function
-        def _recompute_week_number(table, field_name, target_field):
-            env.cr.execute("""
-                UPDATE {table}
-                SET {target_field} = (
-                    1 +
-                    MOD(DIV(ABS(
-                        DATE_PART('day', AGE(%s, {field_name}))
-                    )::integer, 7), %s))::integer
-                WHERE {field_name} IS NOT NULL
-            """.format(
-                table=table,
-                field_name=field_name,
-                target_field=target_field,
-            ), (weekA_date, n_weeks_cycle))
-
+        _recompute_week_number = \
+            env['coop_shift.config.settings']._recompute_week_number
+        # Update pos_session
         _recompute_week_number(
-            table='pos_session',
-            field_name='start_at',
-            target_field='week_number')
+            'pos_session', 'start_at', 'week_number', 'week_name')
+        # Update pos_order
         _recompute_week_number(
-            table='pos_order',
-            field_name='date_order',
-            target_field='week_number')
+            'pos_order', 'date_order', 'week_number', 'week_name')
+        # Update pos_order_line
+        env.cr.execute("""
+            UPDATE pos_order_line pol
+            SET
+                week_number = po.week_number,
+                week_name = po.week_name,
+                cycle = po.cycle
+            FROM pos_order po
+            WHERE pol.order_id = po.id
+        """)
--- intercoop_addons/coop_shift/migrations/9.0.9.0.0/post-migrate.py
+++ intercoop_addons/coop_shift/migrations/9.0.9.0.0/post-migrate.py
@@ -1,19 +1,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp import api, SUPERUSER_ID
 
-import logging
-_logger = logging.getLogger(__name__)
-
 
 def migrate(cr, version):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        # Compute new week_name field
-        _logger.info("Computing week_name in shift.template..")
-        env['shift.template'].with_context(
-            active_test=False,
-        ).search([])._compute_week_name()
-        _logger.info("Computing week_name in shift.shift..")
-        env['shift.shift'].with_context(
-            active_test=False,
-        ).search([])._compute_week_name()
+        # Recompute week name and number
+        _recompute_week_number = \
+            env['coop_shift.config.settings']._recompute_week_number
+        _recompute_week_number(
+            'shift_template', 'start_date', 'week_number', 'week_name')
+        _recompute_week_number(
+            'shift_shift', 'date_without_time', 'week_number', 'week_name')
