6f0d6bdb17887199160383c6d00d38ec1427a95a
Nguyen Minh Chien <chien@trobz.com>
2021-05-25 11:14:49 +0000

    [REF] F#43664 - WallChart adjustment

--- coop_purchase/models/res_partner.py
+++ coop_purchase/models/res_partner.py
@@ -6,18 +6,3 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     show_discount = fields.Boolean("Show discounts on update prices")
-
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form',
-                        toolbar=False, submenu=False):
-        res = super().fields_view_get(view_id=view_id, view_type=view_type,
-                                      toolbar=toolbar, submenu=submenu)
-        if view_type == "form":
-            doc = etree.XML(res["arch"])
-            for node in doc.xpath("//field[@name='debit']"):
-                node.addnext(etree.Element("field", name="show_discount"))
-                res["fields"].update(
-                    self.fields_get(allfields=["show_discount"])
-                )
-            res["arch"] = etree.tostring(doc)
-        return res
