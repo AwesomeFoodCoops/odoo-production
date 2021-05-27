b99791653612db835ee7b788578e88b8a9b7bf6c
Nguyen Minh Chien <chien@trobz.com>
2021-05-27 10:17:01 +0000

    [REF] F#43696 - Changes to Purchasing Info in Product Files

--- coop_purchase/__manifest__.py
+++ coop_purchase/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
 {
     "name": "Coop Purchase",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "category": "Purchase",
     "summary": "Coop Purchase",
     "author": "La Louve",
