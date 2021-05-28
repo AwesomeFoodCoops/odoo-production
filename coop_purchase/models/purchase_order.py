b0d59d239fb220e87bc83733c6e83610a5fedd06
Nguyen Minh Chien <chien@trobz.com>
2021-05-28 09:05:17 +0000

    [REF] Don't copy Vendor Reference from Order to Bill

--- coop_purchase/models/purchase_order.py
+++ coop_purchase/models/purchase_order.py
@@ -8,3 +8,11 @@ class PurchaseOrder(models.Model):
     def button_update_prices(self):
         self.ensure_one()
         return self.env.ref("coop_purchase.supplier_info_update_act").read()[0]
+
+    @api.multi
+    def action_view_invoice(self):
+        result = super(PurchaseOrder, self).action_view_invoice()
+        if result.get('context') and \
+                result['context'].get('default_reference'):
+            del result['context']['default_reference']
+        return result
