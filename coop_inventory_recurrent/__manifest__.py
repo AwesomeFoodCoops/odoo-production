656a1b973027301b404eb2b5e5ac38727f86f1fb
Nguyen Minh Chien <chien@trobz.com>
2021-12-27 02:25:26 +0000

    [ADD]  New Inventory Tools

---
+++ coop_inventory_recurrent/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Coop - Inventory Recurrent",
+    "version": "12.0.1.0.0",
+    "category": "stock",
+    "author": "Trobz",
+    "website": "https://trobz.com",
+    "license": "AGPL-3",
+    "depends": [
+        "coop_inventory",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "view/report_stockinventory_new.xml",
+        "view/view_stock_inventory_category_group.xml",
+        "wizards/view_stock_inventory_recurrent_wizard.xml",
+        "wizards/view_stock_inventory_recurrent_report_wizard.xml",
+    ],
+}
---
+++ coop_inventory_recurrent/models/__init__.py
@@ -0,0 +1,5 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+
+from . import stock_inventory
+from . import stock_inventory_category_group
+from . import stock_inventory_category_group_line
